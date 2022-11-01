a48b3fbd25f570b2484dabba64725e3936b3727d
Israel AyoOluwa Adeleke <izzyfresher@gmail.com>
2022-11-01 17:37:23 +0100

    Initial Commit

---
+++ app/core/config.py
@@ -0,0 +1,24 @@
+from typing import List
+from pydantic import BaseSettings, AnyHttpUrl
+from decouple import config
+
+class Settings(BaseSettings):
+  API_V1_STR: str = "/api/v1"
+  JWT_SECRET_KEY: str = config("JWT_SECRET_KEY", cast=str)
+  JWT_REFRESH_SECRET_KEY: str = config("JWT_REFRESH_SECRET_KEY", cast=str)
+  ALGORITHM = "HS256"
+  ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
+  REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+  BACKEND_CORS_ORIGIN: List[AnyHttpUrl] = []
+  PROJECT_NAME: str = "JOB-APP"
+
+
+
+  #Database
+  MONGO_CONNECTION_STRING: str = config("MONGO_CONNECTION_STRING", cast=str)
+
+  class Config:
+    case_sensitive = True
+
+settings = Settings()
+
